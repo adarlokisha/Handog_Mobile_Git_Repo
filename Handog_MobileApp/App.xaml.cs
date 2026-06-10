76079058c12094d2b9df72bd742a117e20df0008
Kizeah <adarlokisha@gmail.com>
2026-06-10 20:49:45 +0800

    Added .NET MAUI base system files

---
+++ Handog_MobileApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Handog_MobileApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Handog_MobileApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Handog_MobileApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
