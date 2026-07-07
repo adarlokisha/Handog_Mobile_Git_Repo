9c86d6174eac84e0113b18432c89536290d05ef7
Kisha <adarlokisha@gmail.com>
2026-07-07 14:49:18 +0800

    from pull

--- Handog_AdminWeb/obj/Debug/net8.0/Handog_AdminWeb.AssemblyInfo.cs
+++ Handog_AdminWeb/obj/Debug/net8.0/Handog_AdminWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a55f031e40a47f64453eac95fe83c5b6c30897")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f37de260d000956ee3aa395d183d9d277cd9f8b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
