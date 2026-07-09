85b4fa831c87d7335891d71d5b0ee0041d51372b
arkaycodes <rance.loresca@gmail.com>
2026-07-09 22:00:00 +0800

    - GETTING ADMIN COMMIT

--- Handog_AdminWeb/obj/Debug/net8.0/Handog_AdminWeb.AssemblyInfo.cs
+++ Handog_AdminWeb/obj/Debug/net8.0/Handog_AdminWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f81ca9cf6a3d001eae125daf66845b8c98848c7d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c3755ddf3ae0031c2ad3fbf015104e8c7f7c753")]
 [assembly: System.Reflection.AssemblyProductAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
