f41f6777485a18abf6e97cf3dcfb2997c5addb3e
inyourspace <l3abreram@gmail.com>
2026-07-09 21:51:18 +0800

    working admin approval

--- Handog_AdminWeb/obj/Debug/net8.0/Handog_AdminWeb.AssemblyInfo.cs
+++ Handog_AdminWeb/obj/Debug/net8.0/Handog_AdminWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b84e00589e0d788f3e0260f9eaf36aec4b541a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adb72a612fc20affd741a1fc5ca92a9ac028ba3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
