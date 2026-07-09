3b84e00589e0d788f3e0260f9eaf36aec4b541a1
inyourspace <l3abreram@gmail.com>
2026-07-09 21:39:14 +0800

    Event Approved/Rejected function working on both Admin side and Organizer Side. Locale is fixed as well.

--- Handog_AdminWeb/obj/Debug/net8.0/Handog_AdminWeb.AssemblyInfo.cs
+++ Handog_AdminWeb/obj/Debug/net8.0/Handog_AdminWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+101df9e27a75a18fa41cf8a65fb0a2ab6bf6c374")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918e689b43de6248f3b232e3a5d47632da3726bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
