93096c9d39c5aa795c58f8a437da566b157614d0
arkaycodes <rance.loresca@gmail.com>
2026-07-15 14:29:01 +0800

    - COMMIT ALL

--- Handog_AdminWeb/obj/Debug/net8.0/Handog_AdminWeb.AssemblyInfo.cs
+++ Handog_AdminWeb/obj/Debug/net8.0/Handog_AdminWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+312891e3a0574fee015ec12bc2d53c33ec00ef50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77d3ebbadf9a75aab7d636772facc07b8b8d629b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Handog_AdminWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
